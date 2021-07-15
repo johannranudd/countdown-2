33f94b5101c53e8a5bc0e9d9309c433f116e219e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-07-15 20:20:34 +0200

    vgj

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const months = [
 const giveaway = document.querySelector('.giveaway');
 const countDownContainer = document.querySelector('.countdown-container');
 
-const futureDate = new Date(2021, 6, 15, 19, 55);
+let futureDate = new Date(2021, 6, 15, 19, 55);
 
 const year = futureDate.getFullYear();
 let month = futureDate.getMonth();
@@ -39,18 +39,21 @@ giveaway.textContent = `Giveaway Ends On ${weekday}, ${day} ${month}
 ${year} ${hour}:0${minutes}pm`;
 
 // print to DOM
-const futureDateTime = futureDate.getTime();
 
 function setTime() {
     setInterval(function() {
         const today = new Date().getTime();
 
-        let distance = futureDateTime - today;
+        let distance = futureDate.getTime() - today;
         // console.log(distance);
         let daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
         let hoursLeft = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
         let minutesLeft = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
         let secondsLeft = Math.floor(distance % (1000 * 60) / (1000));
+        
+        if (distance < 0) {
+            futureDate.setDate(new Date().getDate() + 9);   
+        }
         
         countDownContainer.innerHTML = `<div class="countdown-box">
         <h3>${daysLeft}</h3>
@@ -69,17 +72,14 @@ function setTime() {
         <p>SECS</p>
     </div>`;
 
-    if (distance <= 0) {
-        futureDate.setDate(today.getDate() + 1);
-    }
     }, 1000)
 }
 
-// window.addEventListener("DOMContentLoaded", function() {
-//     setInterval(function() {
-//         setTime();
-//     }, 1)
-// })
+window.addEventListener("DOMContentLoaded", function() {
+    setInterval(function() {
+        setTime();
+    }, 1)
+})
 
-setTime();
+// setTime();
 
